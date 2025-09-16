1b1d6147aed0f1d1a1f70efd0d63e73c2b9b0a95
Tibbe <tlin.jonas@gmail.com>
2025-09-16 13:37:59 +0200

    Completed log in sequence. created userlist (will need more testing). created login loop and login message method. created CreateUser method

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App 
@@ -15,26 +14,75 @@ public class App
          * currentUser[1] = Pin Code
          * currentUser[2] = Bank Balace
          */
+        Boolean running = true;
+        Boolean loggedIn = false;
+        while(running) 
+        {
+            Login();
+            int Startmenu = userInput.nextInt();
+            userInput.nextLine();
+            switch (Startmenu)
+            {
+                case 1: //Log in
+                {
+                    System.out.println("Type in your username");
+                    String userSearch = userInput.nextLine();
+                    for (int i = 0 ; i < userList.size() ; i++)
+                        {
+                            if (userList.get(i)[0].equals(userSearch))
+                            {
+                                System.out.println("Enter your pin code");
+                                if (userList.get(i)[1].equals(userInput.nextLine())) {
+                                    loggedIn = true;
+                                    break;
+                                }
+                                System.out.println("Wrong Pin Code");
+                            }
+                        }
+                        if (loggedIn == false)
+                        {
+                            System.out.println("User not found");
+                        }
+                    break;
+                }
+                case 2: //Create user
+                {
+                    currentUser = CreateUser(userInput);
+                    userList.add(currentUser);
+                    break;
+                }
+                case 3: //Quit
+                {
+                    System.out.println("See you soon!");
+                    running = false;
+                    break;
+                }
+            }
+            
+            while (loggedIn)
+            {
+                System.out.println("Logged in");
+                loggedIn = false;
+            }
+        }
+        userInput.close();
+    }
+    public static void Login()
+    {
         System.out.println("Welcome to Bank-Castle!\n" +
         "What do you want to do? Insert the corresponding number\n" +
         "1 - Log in\n" +
         "2 - Create a new user\n" +
         "3 - Quit");
-        int Startmenu = userInput.nextInt();
-        switch (Startmenu)
-        {
-            case 1:
-            {
-                System.out.println("Inne i switchen");
-            }
-        }
-            
-        Boolean loggedIn = true;
-        
-        while (loggedIn)
-        {
-        
-        }
-        userInput.close();
+    }
+    public static String[] CreateUser(Scanner userInput)
+    {
+        String[] newUser = new String[3];
+        System.out.println("Type in your name");
+        newUser[0] = userInput.nextLine();
+        System.out.println("Enter your pin code");
+        newUser[1] = userInput.nextLine();
+        newUser[2] = "0";
+        return newUser;
     }
 }
